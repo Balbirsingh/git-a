831cb95ebb4ff7e8a379e59a31fb658b8a60250c
Balbirsingh <luckyrajput89@gmail.com>
2013-04-02 14:07:49 +0530

    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeepppppppppppppie

--- a.rb
+++ a.rb
@@ -7,3 +7,15 @@ test , , , , , ,
 testttttttttttttttttttttttttttttttt
 name = ARGV.first || "World"
 whats up .........today
+# Default is World
+# Author: Jim Weirich
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+puts "Hello, #{name}!"
+
+
+
+
+puts "Hello, #{name}!"
+
