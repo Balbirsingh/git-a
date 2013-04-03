ac4afa6ce615cd1766ceb19b3ff95d1ca222361c
Balbirsingh <luckyrajput89@gmail.com>
2013-04-03 13:38:07 +0530

    the commit

--- lib/a.rb
+++ lib/a.rb
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
+=======
+# Default is World
+
 puts "What's your name"
 my_name = gets.strip
 
 puts "Hello, #{my_name}!"
+>>>>>>> master
+
 
